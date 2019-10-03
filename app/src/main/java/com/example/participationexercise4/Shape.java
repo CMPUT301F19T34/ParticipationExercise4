dbbf1872af9da2416d1fa3b5d324d70fa567f80c
Alex Epp <aepp2@ualberta.ca>
2019-10-02 18:04:25 -0600

    Add colour to Shape

--- app/src/main/java/com/example/participationexercise4/Shape.java
+++ app/src/main/java/com/example/participationexercise4/Shape.java
@@ -3,9 +3,15 @@ package com.example.participationexercise4;
 public abstract class Shape {
     private int x;
     private int y;
+    private String coluur = "blue";
 
     public Shape(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+
+    public Shape(int x, int y, String colour) {
+        this(x, y);
+        this.coluur = colour;
     }
 }
